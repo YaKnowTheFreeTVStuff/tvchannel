2e2a37e0e1ccd3d8bd434eae385373b17f9a5768
SamYung-OfficialAccount <65942509+SamYung-OfficialAccount@users.noreply.github.com>
2020-11-12 21:01:39 +0800

    Update now.js

--- now.js
+++ now.js
@@ -10,7 +10,7 @@ function getVideoLink(channelNo){
 var token = "?channelno="+ parseInt(channelNo) + "&mode=prod&audioCode=&format=HLS&callerReferenceNo=20140702122500";
 var ajaxVideo = "http://d1jithvltpp1l1.cloudfront.net/getLiveURL";
 var returnString = syncRequest("GET", ajaxVideo);
-returnString = returnString.getBody();
+returnString = returnString.getBody().toString();
  console.log(returnString);
 returnString = JSON.parse(returnString).asset.hls.adaptive[0];
 return returnString;
