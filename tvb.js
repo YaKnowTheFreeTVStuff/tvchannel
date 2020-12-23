9a04cb71cfa8338ab447e80b7301a682214bd620
SamYung-OfficialAccount <65942509+SamYung-OfficialAccount@users.noreply.github.com>
2020-12-23 09:33:00 +0800

    Update tvb.js

--- tvb.js
+++ tvb.js
@@ -22,6 +22,7 @@ function getRandom(min, max) {
 }
 function getServerIp(){
 let hkIPAddr = JSON.parse(fs.readFileSync("hkipaddr.json"));
+console.log(hkIPAddr);
 let ipRange = hkIPAddr.hkIPList[getRandom(0,hkIPAddr.hkIPList.length - 1)];
 console.log("Selected IP range: " + ipRange);
 let numbersIp1 = ipRange[0].split(".");
